2cb061a5b46af0af556eab55856991785a6c5701
owen2522 <owentlai@icloud.com>
2017-06-23 17:19:56 -1000

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 function fortuneTeller(){
+	var kids = 4;
+	var partnersName= "yolanda";
+	var geographicLocationm = "L.A.";
+	var jobTitles = "coder"; 
+    console.log("You will be a X in Y, and married to Z with N kids");
+    console.log("you will have " + kids + " you will be married to " + partnersName + " you will live in " + geographicLocationm + "your job will be a " + jobTitles );
+	    }
+fortuneTeller();
 
-}
 
 function ageCalculator(){
 
 }
+console.log('Hello World')
+function eatDinner(){
+console.log("eatDinner");	
+}
+function takeAShower(){
+console.log("takeAShower");	
+}
+function brushTeeth(){
+console.log("brushTeeth");	
+}
+eatDinner();
+takeAShower();
+brushTeeth();
